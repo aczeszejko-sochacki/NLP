7f09a9b5eb52dcf5be76cac11e5e04e0f620c505
aleksander <aleksanderczeso@gmail.com>
2018-12-14 11:23:02 +0100

    Implement generating all possible revised versions of a token

---
+++ list3/src/parsers/token_parser.py
@@ -0,0 +1,55 @@
+from typing import Tuple
+from itertools import product
+import numpy as np
+
+
+class TokenParser:
+    """
+    A class implementing operations
+    on a single tokens
+    """
+
+    pl_uncertain_letters = ('a', 'o', 'e', 'z')
+    sign_possible_alts= {
+        'a': ('a', 'ą'),
+        'e': ('e', 'ę'),
+        'o': ('o', 'ó'),
+        'z': ('z', 'ź', 'ż'),
+     }
+
+    def extract_pl_uncertain_letters(self, token: str) -> Tuple:
+        """
+        Return (letter, index of letter) pairs
+        for all uncertain letter in the token
+        """
+
+        uncertain_letters = [(ind, letter) for ind, letter in enumerate(token)
+                             if letter in self.pl_uncertain_letters]
+
+        return uncertain_letters
+
+    def gen_possible_revised_tokens(self, token: str) -> Tuple:
+        """ """
+
+        # Get uncertain letters and their offsets
+        inds, letters = tuple(zip(*self.extract_pl_uncertain_letters(token)))
+
+        letter_sets = [self.sign_possible_alts[letter] for letter in letters]
+
+        revised_letters_all = tuple(product(*letter_sets))
+
+        # Delete uncertain letters
+        revised_token_core = list(token)
+        np.delete(revised_token_core, inds).tolist()
+
+        revised_tokens = []
+
+        for alt_letters in revised_letters_all:
+            core = np.array(revised_token_core)
+            print(core)
+
+            np.put(core, inds, alt_letters)
+
+            revised_tokens.append(core)
+
+        return revised_tokens
